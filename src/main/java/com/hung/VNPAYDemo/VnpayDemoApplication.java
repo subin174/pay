307f96e970ee6a745d5571765c36c521eca3bef7
DaoPhuHung <daophuhung174@gmail.com>
2024-04-25 11:04:01 +0700

    first commit

---
+++ src/main/java/com/hung/VNPAYDemo/VnpayDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hung.VNPAYDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnpayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnpayDemoApplication.class, args);
+	}
+
+}
